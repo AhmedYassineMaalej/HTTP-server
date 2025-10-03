225c1afb96c363c79c23e7551a500176a69158f8
AhmedYassineMaalej <ahmedyessinemaalej@gmail.com>
2025-10-03 13:55:40 +0100

    added state to reponse writer

--- src/response.rs
+++ src/response.rs
@@ -39,28 +39,54 @@ impl From<StatusCode> for StatusLine {
     }
 }
 
+#[derive(Debug, PartialEq)]
+enum ResponseWriterState {
+    Init,
+    StatusLine,
+    Headers,
+    Body,
+}
+
 pub struct ResponseWriter<'a> {
     inner: BufWriter<&'a mut TcpStream>,
+    state: ResponseWriterState,
 }
 
 impl<'a> ResponseWriter<'a> {
     pub fn write_status_line(&mut self, status_line: &StatusLine) {
+        if self.state != ResponseWriterState::Init {
+            todo!()
+        }
+
         let status_line = format!("HTTP/1.1 {} {}\r\n", status_line.code, status_line.message);
         self.inner.write_all(status_line.as_bytes()).unwrap();
+        self.state = ResponseWriterState::StatusLine;
     }
 
     pub fn write_headers(&mut self, headers: &Headers) {
+        if self.state != ResponseWriterState::StatusLine {
+            todo!()
+        }
         for (key, val) in headers.iter() {
             write!(self.inner, "{key}: {val}\r\n").unwrap();
         }
+
         write!(self.inner, "\r\n").unwrap();
+        self.state = ResponseWriterState::Headers;
     }
 
     pub fn write_body(&mut self, body: &[u8]) {
+        if self.state != ResponseWriterState::Headers {
+            todo!()
+        }
         self.inner.write_all(body).unwrap();
+        self.state = ResponseWriterState::Body;
     }
 
     pub fn new(writer: BufWriter<&'a mut TcpStream>) -> Self {
-        Self { inner: writer }
+        Self {
+            inner: writer,
+            state: ResponseWriterState::Init,
+        }
     }
 }
